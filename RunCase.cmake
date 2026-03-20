1d53b5c9ab2fe3109f1289fbafdfb9add03dc1b2
tim.wu <xray0h@gmail.com>
2026-03-20 15:24:28 +0800

    pipe clang instead of gen .ll

--- RunCase.cmake
+++ RunCase.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.20)
 
-foreach(v COMPILER CLANG SRC OUT_LL OUT_BIN ROOT)
+foreach(v COMPILER CLANG SRC OUT_BIN ROOT)
   if(NOT DEFINED ${v} OR "${${v}}" STREQUAL "")
     message(FATAL_ERROR "Missing required -D${v}=...")
   endif()
@@ -13,29 +13,34 @@ else()
   set(expected_output "")
 endif()
 
-get_filename_component(out_ll_dir "${OUT_LL}" DIRECTORY)
 get_filename_component(out_bin_dir "${OUT_BIN}" DIRECTORY)
-file(MAKE_DIRECTORY "${out_ll_dir}")
 file(MAKE_DIRECTORY "${out_bin_dir}")
 
-execute_process(
-  COMMAND "${COMPILER}" "${SRC}" -o "${OUT_LL}"
-  RESULT_VARIABLE front_rc
-  OUTPUT_VARIABLE front_out
-  ERROR_VARIABLE front_err
-)
-if(NOT front_rc EQUAL 0)
-  message(FATAL_ERROR "[FRONTEND_FAIL] ${SRC}\n${front_err}")
+find_program(BASH_EXECUTABLE NAMES bash)
+if(NOT BASH_EXECUTABLE)
+  message(FATAL_ERROR "bash is required for piped test execution")
 endif()
 
 execute_process(
-  COMMAND "${CLANG}" "${OUT_LL}" -o "${OUT_BIN}" -lm
-  RESULT_VARIABLE back_rc
-  OUTPUT_VARIABLE back_out
-  ERROR_VARIABLE back_err
+  COMMAND "${BASH_EXECUTABLE}" "-lc"
+          "front_err=$(mktemp); back_err=$(mktemp); \
+           \"${COMPILER}\" \"${SRC}\" 2>\"\${front_err}\" | \"${CLANG}\" -x ir - -o \"${OUT_BIN}\" -lm 2>\"\${back_err}\"; \
+           st_front=\${PIPESTATUS[0]}; st_back=\${PIPESTATUS[1]}; \
+           if [ \${st_front} -ne 0 ]; then cat \"\${front_err}\"; rm -f \"\${front_err}\" \"\${back_err}\"; exit 101; fi; \
+           if [ \${st_back} -ne 0 ]; then cat \"\${back_err}\"; rm -f \"\${front_err}\" \"\${back_err}\"; exit 102; fi; \
+           rm -f \"\${front_err}\" \"\${back_err}\""
+  RESULT_VARIABLE pipe_rc
+  OUTPUT_VARIABLE pipe_out
+  ERROR_VARIABLE pipe_err
 )
-if(NOT back_rc EQUAL 0)
-  message(FATAL_ERROR "[BACKEND_FAIL] ${SRC}\n${back_err}")
+if(pipe_rc EQUAL 101)
+  message(FATAL_ERROR "[FRONTEND_FAIL] ${SRC}\n${pipe_out}${pipe_err}")
+endif()
+if(pipe_rc EQUAL 102)
+  message(FATAL_ERROR "[BACKEND_FAIL] ${SRC}\n${pipe_out}${pipe_err}")
+endif()
+if(NOT pipe_rc EQUAL 0)
+  message(FATAL_ERROR "[COMPILE_PIPE_FAIL] ${SRC}\n${pipe_out}${pipe_err}")
 endif()
 
 execute_process(
